6ce10a4f24f0e9a291910e989be1fd3343d6298d
Hunter2065 <vanuha277@gmail.com>
2021-12-19 13:39:35 +0200

    remove env variable

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,6 @@ set :repo_url, "git@github.com:Hunter2065/test_capistrano.git"
 
 # Deploy to the user's home directory
 set :deploy_to, "/home/xi-master/#{fetch :application}"
-set :default_env, { 'PASSENGER_INSTANCE_REGISTRY_DIR' => '/home/xi-master/.rvm/gems/ruby-3.0.0/bin/passenger' }
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
