a5ab61973982a5fde39fdc94cf924efe5ee12308
Hunter2065 <vanuha277@gmail.com>
2021-12-19 12:59:46 +0200

    change path

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@ set :repo_url, "git@github.com:Hunter2065/test_capistrano.git"
 
 # Deploy to the user's home directory
 set :deploy_to, "/home/xi-master/#{fetch :application}"
-set :default_env, { 'PASSENGER_INSTANCE_REGISTRY_DIR' => '/opt/nginx/passenger_temp' }
+set :default_env, { 'PASSENGER_INSTANCE_REGISTRY_DIR' => '/home/xi-master/.rvm/gems/ruby-3.0.0/gems/passenger-6.0.12/bin/passenger' }
 
 append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
 
